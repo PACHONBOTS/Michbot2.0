382bad497556c4a420c6c7d898be346d0dd22fc4
PACHONBOTS <142656807+PACHONBOTS@users.noreply.github.com>
2024-01-31 01:48:44 -0600

    Update grupos-admin.js

--- plugins/grupos-admin.js
+++ plugins/grupos-admin.js
@@ -170,8 +170,8 @@ let pesan = args.join` `
 let oi = `» ${lenguajeGB['smsAddB5']()} ${pesan}`
 let teks = `${lenguajeGB['smstagaa']()}\n\n${oi}\n\n`
 for (let mem of participants) {
-teks += `» 👤 @${mem.id.split('@')[0]}\n`}
-teks += `𝙉𝙖𝙣𝙞 𝘽𝙤𝙩 𝙈𝙖𝙭`
+teks += `┣➥😺@${mem.id.split('@')[0]}\n`}
+teks += `MICH BOT`
 await conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )    
 break
     
